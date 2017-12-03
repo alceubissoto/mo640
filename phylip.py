902d96c5e7b3f3874f9568028d09b59e5457953f
Tanaka Edgar-TANAKAED <tanakaed@motorola.com>
2017-12-03 19:36:47 -0200

    prep

--- phylip.py
+++ phylip.py
@@ -81,7 +81,7 @@ def run_phylip(args):
             shutil.copy(file_path, 'infile')
 
             # run fitch
-            os.system('./phylip_linux/exe/fitch < input_fitch')
+            os.system('./phylip_linux/phylip-3.696/exe/fitch < input_fitch')
 
             # copy out outputfile / outputtree
             fitch_dist_matrix = get_dist_matrix_from_tree('outtree')
